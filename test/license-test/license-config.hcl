7c80a6c1c9fa9ee7447a480c1472fd1c2243ba08
Brandon <wagnerbm@amazon.com>
2022-03-18 15:39:46 -0500

    fix tests

--- test/license-test/license-config.hcl
+++ test/license-test/license-config.hcl
@@ -1,9 +1,9 @@
 allow = [
-   "MIT", 
+   "MIT",
    "Apache-2.0",
    "BSD-3-Clause",
-   "ISC",
-   "MPL-2.0"
+   "BSD-2-Clause",
+   "ISC"
 ]
 deny = [
    "GNU General Public License v2.0"
@@ -14,5 +14,5 @@ translate = {
 override = {
   "sigs.k8s.io/yaml" = "MIT",
   "github.com/gogo/protobuf" = "BSD-3-Clause"
-  "github.com/ghodss/yaml" = "MIT"
+  "github.com/russross/blackfriday" = "BSD-2-Clause"
 }
--- test/license-test/run-license-test.sh
+++ test/license-test/run-license-test.sh
@@ -2,15 +2,14 @@
 set -euo pipefail
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-BUILD_BIN="$SCRIPTPATH/../../build/bin"
+BUILD_PATH="$SCRIPTPATH/../../build"
+BUILD_BIN="$BUILD_PATH/bin"
 
-OS=$(uname | tr '[:upper:]' '[:lower:]')
-ARCH=amd64
-BINARY_NAME="ec2-instance-selector-$OS-$ARCH"
-LICENSE_TEST_TAG="aeis-license-test"
+BINARY_NAME="node-termination-handler-linux-amd64"
+LICENSE_TEST_TAG="nth-license-test"
+LICENSE_REPORT_FILE="$BUILD_PATH/license-report"
 
-SUPPORTED_PLATFORMS="$OS/$ARCH" make -f $SCRIPTPATH/../../Makefile build-binaries
-
-SUPPORTED_PLATFORMS="$OS/$ARCH" make -s -f $SCRIPTPATH/../../Makefile build-binaries
-docker build --build-arg=GOPROXY=direct -t $LICENSE_TEST_TAG $SCRIPTPATH/
-docker run -i -e GITHUB_TOKEN --rm -v $SCRIPTPATH/:/test -v $BUILD_BIN/:/aeis-bin $LICENSE_TEST_TAG golicense /test/license-config.hcl /aeis-bin/$BINARY_NAME
+SUPPORTED_PLATFORMS_LINUX="linux/amd64" make -s -f $SCRIPTPATH/../../Makefile build-binaries
+docker buildx build --load --build-arg=GOPROXY=direct -t $LICENSE_TEST_TAG $SCRIPTPATH/
+docker run -i -e GITHUB_TOKEN --rm -v $SCRIPTPATH/:/test -v $BUILD_BIN/:/nth-bin $LICENSE_TEST_TAG golicense /test/license-config.hcl /nth-bin/$BINARY_NAME | tee $LICENSE_REPORT_FILE
+$SCRIPTPATH/check-licenses.sh $LICENSE_REPORT_FILE
