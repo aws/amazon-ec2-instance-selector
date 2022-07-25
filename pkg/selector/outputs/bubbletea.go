a58e05b6bb3adeea7f91acf980b3e89740dca2e7
Rodrigo Okamoto <rodocp@amazon.com>
2022-07-25 17:14:11 -0500

    created instance type view

--- pkg/selector/outputs/bubbletea.go
+++ pkg/selector/outputs/bubbletea.go
@@ -15,15 +15,19 @@ package outputs
 
 import (
 	"fmt"
+	"math"
 	"strings"
 
 	"github.com/aws/amazon-ec2-instance-selector/v2/pkg/instancetypes"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/evertras/bubble-table/table"
+	"github.com/muesli/termenv"
 )
 
 const (
+	// table formatting
 	rowsPerPage  = 10
 	headerBuffer = 2
 	maxWidth     = 140
@@ -47,7 +51,11 @@ const (
 	colKeySpotPrice          = "Spot Price/Hr (30 day avg)"
 
 	// controls
-	controlsString = "Controls: ↑/↓ - up/down • ←/→  - left/right • z/x - pg up/down • q - quit"
+	controlsString = "Controls: ↑/↓ - up/down • ←/→  - left/right • shift + ←/→ - pg up/down • q - quit"
+
+	// table states
+	tableState   = "table"
+	verboseState = "verbose"
 )
 
 var (
@@ -72,9 +80,40 @@ var (
 	}
 )
 
+// styling for viewport
+var (
+	titleStyle = func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		b.Right = "├"
+		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 1)
+	}()
+
+	infoStyle = func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		b.Left = "┤"
+		return titleStyle.Copy().BorderStyle(b)
+	}()
+)
+
 // BubbleTeaModel is used to hold the state of the bubble tea TUI
 type BubbleTeaModel struct {
+	instanceTypes []*instancetypes.Details
+
+	// holds the output state of the model
+	// TODO: Instead of a string maybe have a "active model"?
+	// and then implement a model for each state? Like the verbose printout
+	// state would have its own update and view methods?
+	// This could be easier to expand perhaps?
+	state string
+
+	// the model for the table output
 	tableModel table.Model
+
+	// verbose output
+	focusedInstance string
+
+	// model for verbose output viewport
+	verboseViewport viewport.Model
 }
 
 // createRows creates a row for each instance type in the passed in list
@@ -139,6 +178,10 @@ func createRows(instanceTypes []*instancetypes.Details) []table.Row {
 // NewBubbleTeaModel initializes a new bubble tea Model which represents
 // a stylized table to display instance types
 func NewBubbleTeaModel(instanceTypes []*instancetypes.Details) BubbleTeaModel {
+	// TODO: clear up how this also makes verbose output
+
+	// TODO: maybe move table creation to helper method
+
 	// create columns based on column names
 	columns := []table.Column{
 		table.NewColumn(colKeyInstanceType, colKeyInstanceType, len(colKeyInstanceType)+headerBuffer),
@@ -161,13 +204,24 @@ func NewBubbleTeaModel(instanceTypes []*instancetypes.Details) BubbleTeaModel {
 	rows := createRows(instanceTypes)
 
 	// set keys for traversing table
+	// TODO: change from having default key map to just the ones listed
 	keys := table.DefaultKeyMap()
 	keys.ScrollLeft.SetKeys("left")
 	keys.ScrollRight.SetKeys("right")
-	keys.PageDown.SetKeys("x", "l", "pgdown")
-	keys.PageUp.SetKeys("z", "h", "pgup")
+	keys.PageDown.SetKeys("x", "pgdown", "shift+right")
+	keys.PageUp.SetKeys("z", "pgup", "shift+left")
+	keys.RowUp.SetKeys("up")
+	keys.RowDown.SetKeys("down")
+
+	// TODO: create verbose output model
+	// probably move this somewhere else
+	// TODO: have different constant for height
+	viewportModel := viewport.New(maxWidth, rowsPerPage)
+	viewportModel.MouseWheelEnabled = true
 
 	model := BubbleTeaModel{
+		instanceTypes: instanceTypes,
+		state:         tableState,
 		tableModel: table.New(columns).
 			WithRows(rows).
 			WithKeyMap(keys).
@@ -187,6 +241,7 @@ func NewBubbleTeaModel(instanceTypes []*instancetypes.Details) BubbleTeaModel {
 				Data:  "Not Fetched",
 			}).
 			HeaderStyle(lipgloss.NewStyle().Align(lipgloss.Center).Bold(true)),
+		verboseViewport: viewportModel,
 	}
 
 	return model
@@ -200,6 +255,10 @@ func (m BubbleTeaModel) Init() tea.Cmd {
 // Update is used by bubble tea to update the state of the bubble
 // tea model based on user input
 func (m BubbleTeaModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	// TODO: check message for screen resizing
+	//		maybe can fix bug with printing out verbose output not
+	//		clearing screen correctly
+
 	// check for quit
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
@@ -209,24 +268,126 @@ func (m BubbleTeaModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	}
 
-	// update table
-	var cmd tea.Cmd
-	m.tableModel, cmd = m.tableModel.Update(msg)
+	// check view state
+	switch m.state {
+	case tableState:
+		// update table
+		var cmd tea.Cmd
+		m.tableModel, cmd = m.tableModel.Update(msg)
 
-	// update footer
-	controlsStr := lipgloss.NewStyle().Faint(true).Render(controlsString)
-	footerStr := fmt.Sprintf("Page: %d/%d | %s", m.tableModel.CurrentPage(), m.tableModel.MaxPages(), controlsStr)
-	m.tableModel = m.tableModel.WithStaticFooter(footerStr)
+		// update footer
+		controlsStr := lipgloss.NewStyle().Faint(true).Render(controlsString)
+		footerStr := fmt.Sprintf("Page: %d/%d | %s", m.tableModel.CurrentPage(), m.tableModel.MaxPages(), controlsStr)
+		m.tableModel = m.tableModel.WithStaticFooter(footerStr)
 
-	return m, cmd
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			// check for change in state
+			switch msg.String() {
+			case "enter", "c":
+				// clear screen
+				//termenv.ClearScreen()
+
+				// change to verbose state
+				m.state = verboseState
+				rowIndex := m.tableModel.GetHighlightedRowIndex()
+				focusedInstance := m.instanceTypes[rowIndex]
+
+				// TODO: cleanup? maybe with substring to remove new line char
+				m.focusedInstance = *focusedInstance.InstanceType
+				m.verboseViewport.SetContent(VerboseInstanceTypeOutput([]*instancetypes.Details{focusedInstance})[0])
+
+				// move viewport to top of printout
+				m.verboseViewport.SetYOffset(0)
+			}
+		case tea.WindowSizeMsg:
+			// handle screen resizing
+			// TODO: use to handle bubble tea bug where
+			// printing outside of screen bounds causes print bugs
+			termenv.ClearScreen()
+
+			// handle width changes
+			if msg.Width < maxWidth {
+				m.tableModel = m.tableModel.WithMaxTotalWidth(msg.Width)
+			} else {
+				m.tableModel = m.tableModel.WithMaxTotalWidth(maxWidth)
+			}
+
+			// TODO: handle height changes?
+		}
+
+		return m, cmd
+	case verboseState:
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch msg.String() {
+			case "enter", "c":
+				// clear screen
+				// TODO: use to handle bubble tea bug where
+				// printing outside of screen bounds causes print bugs
+				//termenv.ClearScreen()
+
+				// change to table state
+				m.state = tableState
+			}
+		case tea.WindowSizeMsg:
+			// handle screen resizing
+			// TODO: use to handle bubble tea bug where
+			// printing outside of screen bounds causes print bugs
+			termenv.ClearScreen()
+
+			// handle width changes
+			if msg.Width < maxWidth {
+				m.verboseViewport.Width = msg.Width
+			} else {
+				m.verboseViewport.Width = maxWidth
+			}
+
+			// TODO: handle height changes?
+		}
+
+		// TODO: clean up
+		// update viewport model
+		var cmd tea.Cmd
+		m.verboseViewport, cmd = m.verboseViewport.Update(msg)
+		return m, cmd
+	}
+
+	return m, nil
 }
 
 // View is used by bubble tea to render the bubble tea model
 func (m BubbleTeaModel) View() string {
 	outputStr := strings.Builder{}
 
-	outputStr.WriteString(m.tableModel.View())
-	outputStr.WriteString("\n")
+	switch m.state {
+	case tableState:
+		outputStr.WriteString(m.tableModel.View())
+		outputStr.WriteString("\n")
+	case verboseState:
+		// TODO: instead of clearing screen on update, look into viewport for
+		// verbose printout
+
+		// for _, str := range m.focusedInstanceOutput {
+		// 	outputStr.WriteString(str)
+		// 	outputStr.WriteString("\n")
+		// }
+
+		// format header for viewport
+		instanceName := titleStyle.Render(m.focusedInstance)
+		line := strings.Repeat("─", int(math.Max(0, float64(m.verboseViewport.Width-lipgloss.Width(instanceName)))))
+		outputStr.WriteString(lipgloss.JoinHorizontal(lipgloss.Center, instanceName, line))
+		outputStr.WriteString("\n")
+
+		outputStr.WriteString(m.verboseViewport.View())
+		outputStr.WriteString("\n")
+
+		// format footer for viewport
+		pagePercentage := infoStyle.Render(fmt.Sprintf("%3.f%%", m.verboseViewport.ScrollPercent()*100))
+		line = strings.Repeat("─", int(math.Max(0, float64(m.verboseViewport.Width-lipgloss.Width(pagePercentage)))))
+		outputStr.WriteString(lipgloss.JoinHorizontal(lipgloss.Center, line, pagePercentage))
+		outputStr.WriteString("\n")
+	}
 
 	return outputStr.String()
 }
