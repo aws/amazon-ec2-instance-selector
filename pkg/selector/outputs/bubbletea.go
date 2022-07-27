dbcb2588b4aa8fc8d8b4d154919a3e93ebea2845
Rodrigo Okamoto <rodocp@amazon.com>
2022-07-27 14:51:02 -0500

    cleaned up table creation code + vertical scaling

--- pkg/selector/outputs/bubbletea.go
+++ pkg/selector/outputs/bubbletea.go
@@ -15,11 +15,10 @@ package outputs
 
 import (
 	"fmt"
-	"math"
 	"strings"
 
 	"github.com/aws/amazon-ec2-instance-selector/v2/pkg/instancetypes"
-	"github.com/charmbracelet/bubbles/viewport"
+	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/evertras/bubble-table/table"
@@ -28,9 +27,8 @@ import (
 
 const (
 	// table formatting
-	rowsPerPage  = 10
-	headerBuffer = 2
-	maxWidth     = 140
+	headerAndFooterPadding = 7
+	headerBuffer           = 2
 )
 
 const (
@@ -52,10 +50,6 @@ const (
 
 	// controls
 	controlsString = "Controls: ↑/↓ - up/down • ←/→  - left/right • shift + ←/→ - pg up/down • q - quit"
-
-	// table states
-	tableState   = "table"
-	verboseState = "verbose"
 )
 
 var (
@@ -80,48 +74,85 @@ var (
 	}
 )
 
-// styling for viewport
-var (
-	titleStyle = func() lipgloss.Style {
-		b := lipgloss.RoundedBorder()
-		b.Right = "├"
-		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 1)
-	}()
-
-	infoStyle = func() lipgloss.Style {
-		b := lipgloss.RoundedBorder()
-		b.Left = "┤"
-		return titleStyle.Copy().BorderStyle(b)
-	}()
-)
-
 // BubbleTeaModel is used to hold the state of the bubble tea TUI
 type BubbleTeaModel struct {
-	instanceTypes []*instancetypes.Details
-
-	// holds the output state of the model
-	// TODO: Instead of a string maybe have a "active model"?
-	// and then implement a model for each state? Like the verbose printout
-	// state would have its own update and view methods?
-	// This could be easier to expand perhaps?
-	state string
-
 	// the model for the table output
 	tableModel table.Model
-
-	// verbose output
-	focusedInstance string
-
-	// model for verbose output viewport
-	verboseViewport viewport.Model
 }
 
+// NewBubbleTeaModel initializes a new bubble tea Model which represents
+// a stylized table to display instance types
+func NewBubbleTeaModel(instanceTypes []*instancetypes.Details) BubbleTeaModel {
+	return BubbleTeaModel{
+		tableModel: createTable(instanceTypes),
+	}
+}
+
+// Init is used by bubble tea to initialize a bubble tea table
+func (m BubbleTeaModel) Init() tea.Cmd {
+	return nil
+}
+
+// Update is used by bubble tea to update the state of the bubble
+// tea model based on user input
+func (m BubbleTeaModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	// check for quit
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "esc", "q":
+			return m, tea.Quit
+		}
+	case tea.WindowSizeMsg:
+		// handle screen resizing
+
+		// This is needed to handle a bug with bubble tea
+		// where resizing causes misprints (https://github.com/Evertras/bubble-table/issues/121)
+		termenv.ClearScreen()
+
+		// handle width changes
+		m.tableModel = m.tableModel.WithMaxTotalWidth(msg.Width)
+
+		// handle height changes
+		if headerAndFooterPadding >= msg.Height {
+			// height too short to fit rows
+			m.tableModel = m.tableModel.WithPageSize(0)
+		} else {
+			newRowsPerPage := msg.Height - headerAndFooterPadding
+			m.tableModel = m.tableModel.WithPageSize(newRowsPerPage)
+		}
+	}
+
+	// update table
+	var cmd tea.Cmd
+	m.tableModel, cmd = m.tableModel.Update(msg)
+
+	// update footer
+	controlsStr := lipgloss.NewStyle().Faint(true).Render(controlsString)
+	footerStr := fmt.Sprintf("Page: %d/%d | %s", m.tableModel.CurrentPage(), m.tableModel.MaxPages(), controlsStr)
+	m.tableModel = m.tableModel.WithStaticFooter(footerStr)
+
+	return m, cmd
+}
+
+// View is used by bubble tea to render the bubble tea model
+func (m BubbleTeaModel) View() string {
+	outputStr := strings.Builder{}
+
+	outputStr.WriteString(m.tableModel.View())
+	outputStr.WriteString("\n")
+
+	return outputStr.String()
+}
+
+// table creation helpers:
+
 // createRows creates a row for each instance type in the passed in list
-func createRows(instanceTypes []*instancetypes.Details) []table.Row {
+func createRows(instanceTypes []*instancetypes.Details) *[]table.Row {
 	rows := []table.Row{}
 
-	none := "none"
 	for _, instanceType := range instanceTypes {
+		none := "none"
 		hyperisor := instanceType.Hypervisor
 		if hyperisor == nil {
 			hyperisor = &none
@@ -172,15 +203,13 @@ func createRows(instanceTypes []*instancetypes.Details) []table.Row {
 		rows = append(rows, newRow)
 	}
 
-	return rows
+	return &rows
 }
 
-// NewBubbleTeaModel initializes a new bubble tea Model which represents
-// a stylized table to display instance types
-func NewBubbleTeaModel(instanceTypes []*instancetypes.Details) BubbleTeaModel {
-	// TODO: clear up how this also makes verbose output
-
-	// TODO: maybe move table creation to helper method
+// createColumns creates columns based on the column key constants
+func createColumns() *[]table.Column {
+	// GPU info field has long strings, so have different buffer length for that field
+	gpuInfoBuffer := 10
 
 	// create columns based on column names
 	columns := []table.Column{
@@ -195,216 +224,59 @@ func NewBubbleTeaModel(instanceTypes []*instancetypes.Details) BubbleTeaModel {
 		table.NewColumn(colKeyENI, colKeyENI, len(colKeyENI)+headerBuffer),
 		table.NewColumn(colKeyGPU, colKeyGPU, len(colKeyGPU)+headerBuffer),
 		table.NewColumn(colKeyGPUMemory, colKeyGPUMemory, len(colKeyGPUMemory)+headerBuffer),
-		table.NewColumn(colKeyGPUInfo, colKeyGPUInfo, len(colKeyGPUInfo)+10),
+		table.NewColumn(colKeyGPUInfo, colKeyGPUInfo, len(colKeyGPUInfo)+gpuInfoBuffer),
 		table.NewColumn(colKeyODPrice, colKeyODPrice, len(colKeyODPrice)+headerBuffer),
 		table.NewColumn(colKeySpotPrice, colKeySpotPrice, len(colKeySpotPrice)+headerBuffer),
 	}
 
-	// create rows based on instance type details
-	rows := createRows(instanceTypes)
-
-	// set keys for traversing table
-	// TODO: change from having default key map to just the ones listed
-	keys := table.DefaultKeyMap()
-	keys.ScrollLeft.SetKeys("left")
-	keys.ScrollRight.SetKeys("right")
-	keys.PageDown.SetKeys("x", "pgdown", "shift+right")
-	keys.PageUp.SetKeys("z", "pgup", "shift+left")
-	keys.RowUp.SetKeys("up")
-	keys.RowDown.SetKeys("down")
-
-	// TODO: create verbose output model
-	// probably move this somewhere else
-	// TODO: have different constant for height
-	viewportModel := viewport.New(maxWidth, rowsPerPage)
-	viewportModel.MouseWheelEnabled = true
-
-	model := BubbleTeaModel{
-		instanceTypes: instanceTypes,
-		state:         tableState,
-		tableModel: table.New(columns).
-			WithRows(rows).
-			WithKeyMap(keys).
-			WithPageSize(rowsPerPage).
-			Focused(true).
-			Border(customBorder).
-			WithMaxTotalWidth(maxWidth).
-			// TODO: fix magic number
-			WithHorizontalFreezeColumnCount(1).
-			WithBaseStyle(
-				lipgloss.NewStyle().
-					Align((lipgloss.Left)),
-			).
-			// TODO: maybe remove this because never used
-			WithMissingDataIndicatorStyled(table.StyledCell{
-				Style: lipgloss.NewStyle().Foreground(lipgloss.Color("#faa")),
-				Data:  "Not Fetched",
-			}).
-			HeaderStyle(lipgloss.NewStyle().Align(lipgloss.Center).Bold(true)),
-		verboseViewport: viewportModel,
-	}
-
-	return model
+	return &columns
 }
 
-// Init is used by bubble tea to initialize a bubble tea table
-func (m BubbleTeaModel) Init() tea.Cmd {
-	return nil
-}
-
-// Update is used by bubble tea to update the state of the bubble
-// tea model based on user input
-func (m BubbleTeaModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	// TODO: check message for screen resizing
-	//		maybe can fix bug with printing out verbose output not
-	//		clearing screen correctly
-
-	// check for quit
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "esc", "q":
-			return m, tea.Quit
-		}
+// createKeyMap creates a KeyMap with the controls for the table
+func createKeyMap() *table.KeyMap {
+	keys := table.KeyMap{
+		RowDown: key.NewBinding(
+			key.WithKeys("down"),
+		),
+		RowUp: key.NewBinding(
+			key.WithKeys("up"),
+		),
+		ScrollLeft: key.NewBinding(
+			key.WithKeys("left"),
+		),
+		ScrollRight: key.NewBinding(
+			key.WithKeys("right"),
+		),
+		PageDown: key.NewBinding(
+			key.WithKeys("shift+right"),
+		),
+		PageUp: key.NewBinding(
+			key.WithKeys("shift+left"),
+		),
 	}
 
-	// check view state
-	switch m.state {
-	case tableState:
-		// update table
-		var cmd tea.Cmd
-		m.tableModel, cmd = m.tableModel.Update(msg)
-
-		// update footer
-		controlsStr := lipgloss.NewStyle().Faint(true).Render(controlsString)
-		footerStr := fmt.Sprintf("Page: %d/%d | %s", m.tableModel.CurrentPage(), m.tableModel.MaxPages(), controlsStr)
-		m.tableModel = m.tableModel.WithStaticFooter(footerStr)
-
-		switch msg := msg.(type) {
-		case tea.KeyMsg:
-			// check for change in state
-			switch msg.String() {
-			case "enter", "c":
-				// clear screen
-				//termenv.ClearScreen()
-
-				// change to verbose state
-				m.state = verboseState
-				rowIndex := m.tableModel.GetHighlightedRowIndex()
-				focusedInstance := m.instanceTypes[rowIndex]
-
-				// TODO: cleanup? maybe with substring to remove new line char
-				m.focusedInstance = *focusedInstance.InstanceType
-				m.verboseViewport.SetContent(VerboseInstanceTypeOutput([]*instancetypes.Details{focusedInstance})[0])
-
-				// move viewport to top of printout
-				m.verboseViewport.SetYOffset(0)
-			}
-		case tea.WindowSizeMsg:
-			// handle screen resizing
-			// TODO: use to handle bubble tea bug where
-			// printing outside of screen bounds causes print bugs
-			termenv.ClearScreen()
-
-			// handle width changes
-			if msg.Width < maxWidth {
-				m.tableModel = m.tableModel.WithMaxTotalWidth(msg.Width)
-			} else {
-				m.tableModel = m.tableModel.WithMaxTotalWidth(maxWidth)
-			}
-
-			// TODO: handle height changes?
-		}
-
-		return m, cmd
-	case verboseState:
-		switch msg := msg.(type) {
-		case tea.KeyMsg:
-			switch msg.String() {
-			case "enter", "c":
-				// clear screen
-				// TODO: use to handle bubble tea bug where
-				// printing outside of screen bounds causes print bugs
-				//termenv.ClearScreen()
-
-				// change to table state
-				m.state = tableState
-			}
-		case tea.WindowSizeMsg:
-			// handle screen resizing
-			// TODO: use to handle bubble tea bug where
-			// printing outside of screen bounds causes print bugs
-			termenv.ClearScreen()
-
-			// handle width changes
-			if msg.Width < maxWidth {
-				m.verboseViewport.Width = msg.Width
-			} else {
-				m.verboseViewport.Width = maxWidth
-			}
-
-			// TODO: handle height changes?
-		}
-
-		// TODO: clean up
-		// update viewport model
-		var cmd tea.Cmd
-		m.verboseViewport, cmd = m.verboseViewport.Update(msg)
-		return m, cmd
-	}
-
-	return m, nil
+	return &keys
 }
 
-// View is used by bubble tea to render the bubble tea model
-func (m BubbleTeaModel) View() string {
-	outputStr := strings.Builder{}
+// createTable creates an intractable table which contains information about all of
+// the given instance types
+func createTable(instanceTypes []*instancetypes.Details) table.Model {
+	// can't get terminal size yet, so set temporary value
+	initialDimensionVal := 30
 
-	switch m.state {
-	case tableState:
-		outputStr.WriteString(m.tableModel.View())
-		outputStr.WriteString("\n")
-	case verboseState:
-		// TODO: instead of clearing screen on update, look into viewport for
-		// verbose printout
+	newTable := table.New(*createColumns()).
+		WithRows(*createRows(instanceTypes)).
+		WithKeyMap(*createKeyMap()).
+		WithPageSize(initialDimensionVal).
+		Focused(true).
+		Border(customBorder).
+		WithMaxTotalWidth(initialDimensionVal).
+		WithHorizontalFreezeColumnCount(1).
+		WithBaseStyle(
+			lipgloss.NewStyle().
+				Align((lipgloss.Left)),
+		).
+		HeaderStyle(lipgloss.NewStyle().Align(lipgloss.Center).Bold(true))
 
-		// for _, str := range m.focusedInstanceOutput {
-		// 	outputStr.WriteString(str)
-		// 	outputStr.WriteString("\n")
-		// }
-
-		// format header for viewport
-		instanceName := titleStyle.Render(m.focusedInstance)
-		line := strings.Repeat("─", int(math.Max(0, float64(m.verboseViewport.Width-lipgloss.Width(instanceName)))))
-		outputStr.WriteString(lipgloss.JoinHorizontal(lipgloss.Center, instanceName, line))
-		outputStr.WriteString("\n")
-
-		outputStr.WriteString(m.verboseViewport.View())
-		outputStr.WriteString("\n")
-
-		// format footer for viewport
-		pagePercentage := infoStyle.Render(fmt.Sprintf("%3.f%%", m.verboseViewport.ScrollPercent()*100))
-		line = strings.Repeat("─", int(math.Max(0, float64(m.verboseViewport.Width-lipgloss.Width(pagePercentage)))))
-		outputStr.WriteString(lipgloss.JoinHorizontal(lipgloss.Center, line, pagePercentage))
-		outputStr.WriteString("\n")
-	}
-
-	return outputStr.String()
+	return newTable
 }
-
-// TODO:
-// Possible idea:
-// - Make it so that you can select rows
-// - when row is selected, table disappears and is
-//   replaced with a detailed description of all of
-//   the information about that one instance type
-//      - similar to verbose output (but with nice ~bubble tea~ stuff)
-// - Can leave this "expanded detailed" look by pressing something?
-//      - will bring you back to the table view at preferibly the same
-//         page you were before
-// - will require changing the model struct to have more state
-// - maybe look into bubble tea cmds
-//      - if not then maybe can just have a flag in this
-//		  file which then is checked in update and view to
-//		  have different results printed and different keys
-//		  listened to
