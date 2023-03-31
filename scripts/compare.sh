32d8c93e7dd2fca40e2d12cc10427ab4289977c0
Abhi Shivaditya <abhshia@amazon.com>
2023-03-31 15:00:24 -0700

    enhancing compare script to take larger nstance types

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-## This script helps compare 2 EC2 instance types
-## dependencies: 
-##     1. vimdiff: https://command-not-found.com/vimdiff
-##     2. EC2 instance selector CLI: https://github.com/aws/amazon-ec2-instance-selector
+# Loop through all the arguments passed to the script
+for arg in "$@"
+do
+    ec2-instance-selector -r us-west-2 -v --allow-list=$1 > /tmp/$1 2>/dev/null
+    if [ ! -f "/tmp/$arg" ]; then
+        # If the file doesn't exist, download
+        ec2-instance-selector -r us-west-2 -v --allow-list=$arg > /tmp/$arg 2>/dev/null
+    fi
+done
 
-ec2-instance-selector -r us-west-2 -v --allow-list=$1 > /tmp/$1 2>/dev/null
-ec2-instance-selector -r us-west-2 -v --allow-list=$2 > /tmp/$2 2>/dev/null
+pushd .
 
-vimdiff /tmp/$1 /tmp/$2
+cd /tmp
+vimdiff "$@"
+
+popd
